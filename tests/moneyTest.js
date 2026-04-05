ed8bd9dfe1d9094762d0fbdb89f4cfa8f06e55c9
Mis Markol <mismarkol903@gmail.com>
2026-04-05 15:17:04 +0300

    add jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,5 +1,7 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
+console.log("suite of formatCurrency tests");
+
 if(formatCurrency(2095) === '20.95'){
     console.log('formateCurrency test passed ');
 }
