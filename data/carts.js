c3e56746d3046d1aa2e7c1741d6e7bccbb6033dc
Mis Markol <mismarkol903@gmail.com>
2026-04-06 00:16:05 +0300

    create authomated tests for addToCart

--- data/carts.js
+++ data/carts.js
@@ -1,17 +1,23 @@
-export let cart=JSON.parse(localStorage.getItem('cart'));//to store on a local storage 
+export let cart;
+loadFromStorage();
 
-if(!cart){
-//cart itms in an arrays 
-        cart=[{
-            productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-            quantity:2,
-            deliveryOptionId:'1'
-        },{
-            productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            quantity:1,
-            deliveryOptionId:'2'
-    }];
+export function loadFromStorage(){
+    cart=JSON.parse(localStorage.getItem('cart'));//to store on a local storage 
+
+    if(!cart){
+    //cart itms in an arrays 
+            cart=[{
+                productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity:2,
+                deliveryOptionId:'1'
+            },{
+                productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity:1,
+                deliveryOptionId:'2'
+        }];
+    }
 }
+
 
 // to save thee changes on your local storage and when refresh or restart,  all the previous actions are not changed 
 export function saveToStorage(){
@@ -49,7 +55,7 @@ export function addToCart(productId){
                 deliveryOptionId:'1'
               });
             }
-saveToStorage();
+        saveToStorage();
 }
 
 export function removeFromCart(productID){
