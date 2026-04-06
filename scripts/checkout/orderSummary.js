42d942ca58a62e26858e2bab8345a443b862733b
Mis Markol <mismarkol903@gmail.com>
2026-04-06 10:40:27 +0300

    create automated test for order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -29,7 +29,8 @@ export function renderOrderSummary(){
       cartSummaryHTML += 
 
     `
-    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container 
+   js-cart-item-container js-cart-item-container-${matchingProduct.id}">
                 <div class="delivery-date">
                   Delivery date: ${dateString}
                 </div>
@@ -45,14 +46,14 @@ export function renderOrderSummary(){
                     <div class="product-price">
                       $${formatCurrency(matchingProduct.priceCents)}
                     </div>
-                    <div class="product-quantity">
+                    <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                       <span>
                         Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                       </span>
                       <span class="update-quantity-link link-primary">
                         Update
                       </span>
-                      <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}" >
+                      <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}" >
                         Delete
                       </span>
                     </div>
@@ -116,6 +117,7 @@ export function renderOrderSummary(){
 
 
     document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+
     document.querySelectorAll('.js-delete-link').forEach((link)=>{
       link.addEventListener('click',()=>{
       const productId= link.dataset.productId;
@@ -124,6 +126,7 @@ export function renderOrderSummary(){
 
       );
       container.remove();
+      renderPaymentSummary();
       });
     });
 
